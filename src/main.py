f371be86fb8d5245119ece311550ca7918ee5bba
Mathieu Delarue <mat.delarue@hotmail.fr>
2024-10-02 19:36:22 +0200

    Added st widgets and dataframes for the data

--- src/main.py
+++ src/main.py
@@ -3,8 +3,27 @@ import pandas as pd
 
 from data_loader import *
 
-# Titre de la page
-st.title("Hello World")
-
+# Load data files into dataframes
 df_PP_recipes = load_data("dataset/PP_recipes.csv.zip")
-st.write(df_PP_recipes.head())
+df_PP_users = load_data("dataset/PP_users.csv.zip")
+df_ingredients = load_data("dataset/ingr_map.pkl")
+
+# Title
+st.write("Bienvenue sur l'application Streamlit de Mangetamain!")
+
+# Provide file options in a selectbox
+file_options = ["Recettes", "Utilisateurs", "Ingrédients"]
+dataframes = {
+    "Recettes": df_PP_recipes,
+    "Utilisateurs": df_PP_users,
+    "Ingrédients": df_ingredients,
+}
+selected_file = st.selectbox(
+    "Quel dataframe souhaitez-vous séléctionner: ", file_options
+)
+
+# Add a button to trigger file loading and display
+if st.button("Afficher le dataframe"):
+    st.write(f"Data du dataframe: {selected_file}")
+    df = dataframes[selected_file]
+    st.write(df.head())
