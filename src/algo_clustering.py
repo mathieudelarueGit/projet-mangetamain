6f7150981664b8e5444ce3260f8ba125e5fabce6
Julien <julienvu50@gmail.com>
2024-10-05 23:03:09 +0200

    Implementation of filtering bio recipes etape 1

--- src/algo_clustering.py
+++ src/algo_clustering.py
@@ -2,12 +2,17 @@ import logging
 import pandas as pd
 import streamlit as st
 from log_config import *
+from data_loader import *
+import kaggle
+
 
 # Get a logger specific to this module
 logger = logging.getLogger(__name__)
 
 #EDA
-def dataset_recipes_study(file_name: str) ->  pd.DataFrame:
+def dataset_study(file_name: str) ->  None:
+    #calling to the function load_data from the file data_loader
+    df=load_data(file_name)
     """
     Loads and analyses a CSV file of recipes.
 
@@ -17,15 +22,13 @@ def dataset_recipes_study(file_name: str) ->  pd.DataFrame:
     Returns:
         pd.DataFrame: The DataFrame containing the data from the CSV file.
     """
-    df=pd.read_csv(file_name,sep="," )
-    #noms des colonnes
-    print(df.columns)
-    #description des données
+    #column names
+    print("Column names:", df.columns)
+    #data description 
     print(df.describe())
-    print(df.dropna)
     #étude des données manquantes
-    print(df.isnull().sum().sum())#0 données manquantes
-    return df
+    print("Number of missing data:", df.isnull().sum().sum())
+    
 
 def filtering_bio_recipes(df: pd.DataFrame)-> pd.DataFrame:
     """
@@ -38,12 +41,30 @@ def filtering_bio_recipes(df: pd.DataFrame)-> pd.DataFrame:
         pd.DataFrame: A DataFrame containing only recipes with tags such as organic’, ‘veggie’, ‘organic’, ‘traditional’, or ‘gluten-free’.
 
     """
-    features_bio = r'\b(bio|veggie|organic|traditional|gluten-free)\b'
-    # Filtrer les recettes avec les tags 'bio' ou 'traditionnel'
-    bio_recipes= df[df['tags'].str.contains(features_bio, case=False, na=False, regex=True)]
-    return bio_recipes
+    df=load_data(df)
+    features_bio = r'\b(bio|veggie|organic|gluten-free|vegan)\b'
+    # filtering recipes with the tags bio, organic, traditional, gluten-free, veggie
+    #bio_recipes= df[df['tags'].str.contains(features_bio, case=False, na=False, regex=True)]
+    key_words = ['vegan', 'bio', 'gluten-free', 'veggie','organic']
+    #show the lines where lines of bio_recipes have these key_words
+    bio_recipes1 = df[df['tags'].isin(key_words)]
+    bio_recipes_list=bio_recipes1["id","name"]
+    return bio_recipes_list
 
-#df_bio=filtering_bio_recipes(df)
+
+# Définir le slug du dataset et le nom du fichier
+dataset_slug = 'shuyangli94/food-com-recipes-and-user-interactions'
+filename = 'RAW_recipes.csv'
+
+# Charger le dataset
+#df = load_kaggle_dataset(dataset_slug, filename)
+
+filename_recipes="dataset/PP_recipes.csv_extracted/PP_recipes.csv"
+filename_users="dataset/PP_users.csv_extracted/PP_users.csv"
+temporary_filename_raw="/RAW_recipes.csv"
+data_recipes=dataset_study(filename_recipes)
+#data_users=dataset_study(filename_users)
+#df_bio=filtering_bio_recipes(temporary_filename_raw)
 #print(df_bio)
 
 
