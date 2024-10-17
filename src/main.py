25e996623a0f377df985f14d2ec4b233f3b75d8b
FX Morel <tulipe0212@gmail.com>
2024-10-17 22:03:55 +0200

    from FX: ajout d'une barre à gauche de démo

--- src/main.py
+++ src/main.py
@@ -24,67 +24,177 @@ df_PP_users = load_data("dataset/PP_users.csv.zip")
 df_ingredients = load_data("dataset/ingr_map.pkl")
 df_RAW_recipes = load_data("dataset/RAW_recipes.csv.zip")
 
-# Title
-st.write("Welcome into Mangetamain! data exploration & analysis project")
+def main()->None:
+    """_summary_: this function deals with the display of the main page of the project
+    """
+    # Title
+    st.write("Welcome into Mangetamain! data exploration & analysis project")
 
-# Layout for columns
-row1_1, row1_2, row1_3, row1_4 = st.columns((3, 2, 2, 4))
+    # Layout for columns
+    row0_1, row0_2, row0_3, row0_4 = st.columns((3, 2, 2, 4))
 
-# A few statistics
-with row1_1:
-    st.write("## Encyclopedia for foodies")
-    st.write(f"Number of recipes: {df_RAW_recipes.shape[0]}")
+    # A few statistics
+    # Titles
+    with row0_1:
+        st.write("## Encyclopedia for foodies")
+    with row0_2:
+        st.write("## Wide community")
+    with row0_3:
+        st.write("## Varied food")
+    with row0_4:
+        font_size = 2
+        st.write("## Most popular recipes")
+        
+    # A few statistics
+    # Fully aligned statistics
+    row1_1, row1_2, row1_3, row1_4 = st.columns((3, 2, 2, 4))
+    
+    with row1_1:
+        st.write(f"Number of recipes: {df_RAW_recipes.shape[0]}")
+    
+    with row1_2:
+        st.write(
+            f"As many as {df_PP_users.shape[0]} users got their hands dirty and shared their recipes"
+        )
+    
+    with row1_3:
+        st.write(f"{df_ingredients.shape[0]} different ingredients are found")
+    
+    with row1_4:
+        font_size = 2
+        st.write(
+            f"<font size={font_size}>{top10_hottest_recipes}</font>", unsafe_allow_html=True
+        )
 
-with row1_2:
-    st.write("## Wide community")
-    st.write(
-        f"As many as {df_PP_users.shape[0]} users got their hands dirty and shared their recipes"
+    # Sidebar
+    st.sidebar.title("“This is the sidebar”")           ## TO DO
+    st.sidebar.markdown("“Let’s start with binary classification!!”") ## TO DO
+
+    # Shows if checkbox is checked, because it's slowing down the app
+    if st.sidebar.checkbox("Show general aspects", True): # Add a checkbox to display into the main page
+        st.subheader("Some general purpose analysis")            ## TO DO
+
+        # Second row of columns for graphs
+        row2_1, row2_2, row2_3 = st.columns(3)
+
+        # Display the graphs
+        with row2_1:
+            st.write("Most recipes are strongly rated:")
+            st.plotly_chart(fig1)
+            st.write("...hence rate will not be a good feature for recommendation.")
+
+        with row2_2:
+            st.write("The website and the database was burning hot until 2011:")
+            st.plotly_chart(fig2)
+            st.write("...from that point on, Instagram probably took over.")
+
+        with row2_3:
+            st.write("Some recipes are too popular to be serious:")
+            st.plotly_chart(fig3)
+            st.write("...but we'll keep away from them as they might be biased.")
+            
+        st.sidebar.subheader("Choose classifier") # Add a subheader to the sidebar
+        
+        
+    classifier = st.sidebar.selectbox("Classifier", # Add a selectbox to the sidebar
+                                      ("Support Vector Machine (SVM)", ## TO DO: Add real classifiers
+                                       "Logistic Regression",          ## TO DO: Add real classifiers
+                                       "Random Forest"))               ## TO DO: Add real classifiers
+
+    if classifier == "Support Vector Machine (SVM)":
+        st.sidebar.subheader("Hyperparameters")
+        st.subheader("Here are the hyperparameters for SVM") ## TO DO: add real features
+        C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key="C")
+        kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key="kernel") 
+        gamma = st.sidebar.radio("Gamma (Kernal coefficient", ("scale", "auto"), key="gamma")
+        metrics = st.sidebar.multiselect("What metrics to plot?",
+                                     ("Confusion Matrix", 
+                                      "ROC Curve", 
+                                      "Precision-Recall Curve"))
+        st.write(f"kernel: {kernel}, C: {C}, gamma: {gamma}")
+
+    # Provide file options in a selectbox
+    file_options = ["Recettes", "Recettes brutes", "Utilisateurs", "Ingrédients"]
+    dataframes = {
+        "Recettes": df_PP_recipes,
+        "Recettes brutes": df_RAW_recipes,
+        "Utilisateurs": df_PP_users,
+        "Ingrédients": df_ingredients,
+    }
+    selected_file = st.selectbox(
+        "Quel dataframe souhaitez-vous séléctionner: ", file_options
     )
 
-with row1_3:
-    st.write("## Varied food")
-    st.write(f"{df_ingredients.shape[0]} different ingredients are found")
+    # Add a button to trigger file loading and display
+    if st.button("Afficher le dataframe"):
+        st.write(f"Data du dataframe: {selected_file}")
+        df = dataframes[selected_file]
+        st.write(df.head())
+        
+if __name__ == "__main__":
+    main() # this prevents the code from running when the module is imported
 
-with row1_4:
-    font_size = 2
-    st.write("## Most popular recipes")
-    st.write(
-        f"<font size={font_size}>{top10_hottest_recipes}</font>", unsafe_allow_html=True
-    )
+# # Title
+# st.write("Welcome into Mangetamain! data exploration & analysis project")
 
-# Second row of columns for graphs
-row2_1, row2_2, row2_3 = st.columns(3)
+# # Layout for columns
+# row1_1, row1_2, row1_3, row1_4 = st.columns((3, 2, 2, 4))
 
-# Display the graphs
-with row2_1:
-    st.write("Most recipes are strongly rated:")
-    st.plotly_chart(fig1)
-    st.write("...hence rate will not be a good feature for recommendation.")
+# # A few statistics
+# with row1_1:
+#     st.write("## Encyclopedia for foodies")
+#     st.write(f"Number of recipes: {df_RAW_recipes.shape[0]}")
 
-with row2_2:
-    st.write("The website and the database was burning hot until 2011:")
-    st.plotly_chart(fig2)
-    st.write("...from that point on, Instagram probably took over.")
+# with row1_2:
+#     st.write("## Wide community")
+#     st.write(
+#         f"As many as {df_PP_users.shape[0]} users got their hands dirty and shared their recipes"
+#     )
 
-with row2_3:
-    st.write("Some recipes are too popular to be serious:")
-    st.plotly_chart(fig3)
-    st.write("...but we'll keep away from them as they might be biased.")
+# with row1_3:
+#     st.write("## Varied food")
+#     st.write(f"{df_ingredients.shape[0]} different ingredients are found")
 
-# Provide file options in a selectbox
-file_options = ["Recettes", "Recettes brutes", "Utilisateurs", "Ingrédients"]
-dataframes = {
-    "Recettes": df_PP_recipes,
-    "Recettes brutes": df_RAW_recipes,
-    "Utilisateurs": df_PP_users,
-    "Ingrédients": df_ingredients,
-}
-selected_file = st.selectbox(
-    "Quel dataframe souhaitez-vous séléctionner: ", file_options
-)
+# with row1_4:
+#     font_size = 2
+#     st.write("## Most popular recipes")
+#     st.write(
+#         f"<font size={font_size}>{top10_hottest_recipes}</font>", unsafe_allow_html=True
+#     )
 
-# Add a button to trigger file loading and display
-if st.button("Afficher le dataframe"):
-    st.write(f"Data du dataframe: {selected_file}")
-    df = dataframes[selected_file]
-    st.write(df.head())
+# # Second row of columns for graphs
+# row2_1, row2_2, row2_3 = st.columns(3)
+
+# # Display the graphs
+# with row2_1:
+#     st.write("Most recipes are strongly rated:")
+#     st.plotly_chart(fig1)
+#     st.write("...hence rate will not be a good feature for recommendation.")
+
+# with row2_2:
+#     st.write("The website and the database was burning hot until 2011:")
+#     st.plotly_chart(fig2)
+#     st.write("...from that point on, Instagram probably took over.")
+
+# with row2_3:
+#     st.write("Some recipes are too popular to be serious:")
+#     st.plotly_chart(fig3)
+#     st.write("...but we'll keep away from them as they might be biased.")
+
+# # Provide file options in a selectbox
+# file_options = ["Recettes", "Recettes brutes", "Utilisateurs", "Ingrédients"]
+# dataframes = {
+#     "Recettes": df_PP_recipes,
+#     "Recettes brutes": df_RAW_recipes,
+#     "Utilisateurs": df_PP_users,
+#     "Ingrédients": df_ingredients,
+# }
+# # selected_file = st.selectbox(
+# #     "Quel dataframe souhaitez-vous séléctionner: ", file_options
+# # )
+
+# # Add a button to trigger file loading and display
+# if st.button("Afficher le dataframe"):
+#     st.write(f"Data du dataframe: {selected_file}")
+#     df = dataframes[selected_file]
+#     st.write(df.head())
